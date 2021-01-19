c63a09b38acefdfe7bf24bc37af95bda9efed287
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-01-19 11:19:32 +0100

    Commit

---
+++ src/Listeners/Client/dms.ts
@@ -0,0 +1,21 @@
+import { Listener } from 'discord-akairo';
+import { Message } from 'discord.js'
+import Client from '../../client/Client';
+
+export default class message extends Listener {
+    client: Client
+    public constructor(client: Client) {
+        super("message", {
+            emitter: "client",
+            event: "message",
+            category: "client"
+        });
+        this.client = client
+    }
+
+    async exec(message: Message) {
+    if (message.author.bot) return;
+    if (message.guild !== null) return;
+    this.client.logger.info(`\x1b[35m[DM]\x1b[32m[AUTHOR]\x1b[34m ${message.author.tag} (${message.author.id}) \x1b[32m[CONTENT]\x1b[34m ${message.content}`)
+    }
+}
