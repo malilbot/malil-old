7b2781af77f0584fc8d94e346a7feda36fb76ce1
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-01-20 14:49:41 +0100

    Commit

--- src/client/EnmapVerbose.ts
+++
@@ -1,8 +0,0 @@
-import chalk from 'chalk';
-/**
- * Called on every query to the Enmap internal database.
- * @param query The SQL query ran
- */
-export default function EnmapVerbose(dbname: string, query: string): void {
-    this.client.logger.info('i', `${chalk.blue('[')}${chalk.blue.bold('DatabaseQuery')}${chalk.blue(']')} ${chalk.underline('Query')} ${chalk.red(dbname)}: ${query}`);
-}
