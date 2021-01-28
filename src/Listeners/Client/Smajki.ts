3900a211aa399b302c012b33d95e67d28fda3c17
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-01-28 17:10:23 +0100

    Smajki

---
+++ src/Listeners/Client/Smajki.ts
@@ -0,0 +1,41 @@
+import { Listener } from 'discord-akairo';
+
+import Client from '../../client/Client';
+
+import { Message } from 'discord.js'
+
+export default class smajki extends Listener {
+    client: Client
+    public constructor(client: Client) {
+        super("smajki", {
+            emitter: "client",
+            event: "message",
+            category: "client"
+        });
+        this.client = client
+    }
+public async exec(message: Message) {
+if(message.guild.id == '701833606280118282') {
+
+if(message.content.toLowerCase().includes('smajki') || message.content.toLowerCase().includes('smajk')) {
+message.react('786270601857728583')
+message.react('786270602151067698')
+message.react('786270602532749352')
+message.react('786270602423173131')
+message.react('786270602860167218')
+message.react('786270603061493770')
+}
+if(message.content.toLowerCase().includes('ege') || message.content.toLowerCase().includes('ege')) {
+message.react('786270668697501730')
+message.react('786270668668796978')
+message.react('786270668023005205')
+message.react('786270667883937852')
+message.react('786270667070636034')
+message.react('786270666726047816')
+    
+}
+
+}else return
+
+
+}}
