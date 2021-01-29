34053870459aaf5d74c1910c1842f58fd69f7e3b
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-01-29 08:16:25 +0100

    Tags are officialy almost done

--- src/Commands/Utility/DTagCommand.ts
+++ src/Commands/Utility/DTagCommand.ts
@@ -2,10 +2,10 @@ import { Command } from "discord-akairo";
 import type { Message, GuildMember, ImageSize, AllowedImageFormat } from "discord.js";
 import { MessageEmbed } from "discord.js";
 
-export default class TagCommand extends Command {
+export default class DTagCommand extends Command {
     public constructor() {
-        super("tag", {
-            aliases: ["tag"],
+        super("dtag", {
+            aliases: ["dtag"],
             category: "Utility",
             quoted: true,
             args: [
@@ -16,10 +16,10 @@ export default class TagCommand extends Command {
                 }
             ],
             description: {
-                content: "",
-                usage: "tag",
+                content: "Run a tag and delete the original website",
+                usage: "dtag",
                 example: [
-                    "tag"
+                    "dtag"
                 ]
             },
             ratelimit: 3,
@@ -28,12 +28,12 @@ export default class TagCommand extends Command {
     }
 
     public async exec(message: Message, { args }) {
+        message.delete().catch(e => message.channel.send("sorry something went wrong: " + e))
         if(!this.client.tags.get(message.guild.id, args)) message.channel.send("Sorry couldnt find that tag")
         const embed = new MessageEmbed()
         .setColor("GREEN")
         .setTitle(args)
         .setDescription(this.client.tags.get(message.guild.id, args))
-        message.reply(embed)
 
     }
 }
