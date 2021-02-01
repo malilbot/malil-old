924ecbdd949fda08bdc8fc920c802cb839d8e737
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-02-01 13:43:24 +0100

    Fixed tag bug

--- src/Commands/Utility/TagCommand.ts
+++ src/Commands/Utility/TagCommand.ts
@@ -1,40 +1,41 @@
 import { Command } from "discord-akairo";
-import type { Message, GuildMember, ImageSize, AllowedImageFormat } from "discord.js";
-import { MessageEmbed } from "discord.js";
+import { MessageEmbed, Message } from "discord.js";
 
 export default class TagCommand extends Command {
-    public constructor() {
-        super("tag", {
-            aliases: ["tag"],
-            category: "Utility",
-            quoted: true,
-            args: [
-                {
-                    id: "args",
-                    type: "array",
-                    match: "rest",
-                }
-            ],
-            description: {
-                content: "",
-                usage: "tag",
-                example: [
-                    "tag"
-                ]
-            },
-            ratelimit: 3,
-            channel: "guild"
-        });
-    }
+	public constructor() {
+		super("tag", {
+			aliases: [
+				"tag"
+			],
+			category: "Utility",
+			quoted: true,
+			args: [
+				{
+					id: "args",
+					type: "array",
+					match: "rest"
+				}
+			],
+			description: {
+				content: "",
+				usage: "tag",
+				example: [
+					"tag"
+				]
+			},
+			ratelimit: 3,
+			channel: "guild"
+		});
+	}
 
-    public async exec(message: Message, { args }) {
-        await this.client.tags.ensure(message.guild.id, {});
-        if(!this.client.tags.get(message.guild.id, args)) message.channel.send("Sorry couldnt find that tag")
-        const embed = new MessageEmbed()
-        .setColor("GREEN")
-        .setTitle(args)
-        .setDescription(this.client.tags.get(message.guild.id, args))
-        message.reply(embed)
-
-    }
+	public async exec(message: Message, { args }) {
+		if (!args) return message.reply("Try again but this time actually mention a tag");
+		await this.client.tags.ensure(message.guild.id, {});
+		if (!this.client.tags.get(message.guild.id, args)) message.channel.send("Sorry couldnt find that tag");
+		const embed = new MessageEmbed()
+			.setColor("GREEN")
+			.setTitle(args)
+			.setDescription(this.client.tags.get(message.guild.id, args));
+		message.reply(embed);
+	}
 }
