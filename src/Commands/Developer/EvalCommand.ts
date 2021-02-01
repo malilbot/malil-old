73d2200858c8170669864373ce88166ecbc55ed8
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-02-01 19:29:49 +0100

    Added the ability to delete a eval

--- src/Commands/Developer/EvalCommand.ts
+++ src/Commands/Developer/EvalCommand.ts
@@ -1,75 +1,107 @@
 import { Command } from "discord-akairo";
-import type { Message } from "discord.js";
-import { MessageEmbed } from "discord.js";
-import util from 'util';
-import { gist } from '../../Utils/Utils'
-import * as db from 'quick.db'
+import { MessageEmbed, Message } from "discord.js";
+import util from "util";
+import { gist } from "../../Utils/Utils";
+import centra from "centra";
+import * as db from "quick.db";
 
 export default class EvalCommand extends Command {
-    public constructor() {
-        super("eval", {
-            aliases: ["eval", "ev"],
-            category: "Developer",
-            description: {
-                content: "Some super javascript code",
-                usage: "eval < javascript >",
-                example: [
-                    "eval message.guild.id"
-                ]
-            },
-            ratelimit: 3,
-            args: [
-                {
-                    id: "code",
-                    type: "string",
-                    match: "rest",
-                    default: "Please input some code"
-                }
-            ],
-            ownerOnly: true,
-            channel: "guild"
-        });
-        
-    }
-    public async exec(message: Message, { code }) {
-//
-function dm(guy, message, client) { return client.users.fetch(guy).then(user => user.send(message)) }
-//
-//
-function channel(chanid, message, client){ return client.channels.fetch(chanid).then(channel => channel.send(message)) }
-//
-        const evalcode = code
-            if(code.includes("--silent")) code = code.replace("--silent", "")
-            if(code.includes("--delete")) code = code.replace("--delete", "") && message.delete()
-        const embed = new MessageEmbed()
-            .setTitle(`${this.client.user.tag}'s Evaled`)
-            .setColor("RED")
-            .addField("🍞 Input", `\`\`\`ts\n${code}\`\`\``)
+	public constructor() {
+		super("eval", {
+			aliases: [
+				"eval",
+				"ev"
+			],
+			category: "Developer",
+			description: {
+				content: "Some super javascript code",
+				usage: "eval < javascript >",
+				example: [
+					"eval message.guild.id"
+				]
+			},
+			ratelimit: 3,
+			args: [
+				{
+					id: "code",
+					type: "string",
+					match: "rest",
+					default: "Please input some code"
+				}
+			],
+			ownerOnly: true,
+			channel: "guild"
+		});
+	}
+	public async exec(message: Message, { code }) {
+		//https://gist.github.com/
+		let gists = "";
+		let field: any = function dm(guy, message, client) {
+			return client.users.fetch(guy).then((user) => user.send(message));
+		};
+		//
+		//
+		function channel(chanid, message, client) {
+			return client.channels.fetch(chanid).then((channel) => channel.send(message));
+		}
+		//
+		const evalcode = code;
+		if (code.includes("--silent")) code = code.replace("--silent", "");
+		if (code.includes("--delete")) code = code.replace("--delete", "") && message.delete();
+		const embed = new MessageEmbed()
+			.setTitle(`${this.client.user.tag}'s Evaled`)
+			.setColor("RED")
+			.addField("🍞 Input", `\`\`\`ts\n${code}\`\`\``);
 
-        try {
+		try {
+			var evaled = await eval(code);
 
-            var evaled = await eval(code);
+			const output = util.inspect(evaled, { depth: 0 });
+			if (output.length > 1024) {
+				gists = await gist("eval.ts", output);
+				embed.addField("🫓 Output", "https://gist.github.com/" + gists);
+				embed.addField("Type", typeof evaled);
+			} else {
+				embed.addField("🫓 Output", `\`\`\`ts\n${output}\`\`\``);
+				embed.addField("Type", typeof evaled);
+			}
+		} catch (e) {
+			const error = e;
+			if (error.length > 1024) {
+				gists = await gist("eval.ts", e);
+				embed.addField("🫓 Error", "https://gist.github.com/" + gists);
+				embed.addField("Type", typeof evaled);
+			} else {
+				embed.addField("🫓 Error", `\`\`\`ts\n${error}\`\`\``);
+				embed.addField("Type", typeof evaled);
+			}
+		}
+		if (evalcode.includes("--silent")) return message.author.send(embed);
+		if (evalcode.includes("--delete")) return;
+		let msg = await message.util.send(embed);
 
-            const output = util.inspect(evaled, { depth: 0});
-            if (output.length > 1024) {
-                embed.addField("🫓 Output", await gist('eval.ts', output));
-                embed.addField("Type", typeof evaled);
-            } else {
-                embed.addField("🫓 Output", `\`\`\`ts\n${output}\`\`\``);
-                embed.addField("Type", typeof evaled);
-            }
-        } catch (e) {
-            const error = (e);
-            if (error.length > 1024) {
-                embed.addField("🫓 Error", await gist('eval.ts', e));
-                embed.addField("Type", typeof evaled);
-            } else {
-                embed.addField("🫓 Error", `\`\`\`ts\n${error}\`\`\``);
-                embed.addField("Type", typeof evaled);
-            }
-        }
-        if(evalcode.includes("--silent")) return message.author.send(embed)
-        if(evalcode.includes("--delete")) return
-        message.util.send(embed);
-    }
+		msg.react("🗑️");
+		msg
+			.awaitReactions((reaction, user) => user.id == message.author.id && reaction.emoji.name == "🗑️", {
+				max: 1,
+				time: 60000
+			})
+			.then((collected) => {
+				if (collected.first().emoji.name == "🗑️") {
+					msg.edit(
+						new MessageEmbed()
+							.setTitle(`${this.client.user.tag}'s Evaled`)
+							.setColor("RED")
+							.addField("🍞 Input", `\`\`\`ts\n${code}\`\`\``)
+							.addField("🫓 Output", `\`\`\`ts\nDeleted :kekw:\`\`\``)
+					);
+					if (gists) {
+						centra(`https://api.github.com/gists/${gists}`, "DELETE")
+							.header("User-Agent", "Malil")
+							.header("Authorization", `token ${process.env.gist}`)
+							.send();
+					}
+				}
+			});
+	}
 }
--- src/Utils/Main/Utils/gist.ts
+++ src/Utils/Main/Utils/gist.ts
@@ -1,22 +1,20 @@
-import centra from 'centra'
+import centra from "centra";
 
-export const gist = async function(name: any, content: any){
-    let files: { [key: string]: { content: string } } = {};
-      files[name] = {
-        content: content || 'oops something went wrong :(',
-      };
-    const body = {
-      description: `Malil`,
-      public: false,
-      files,
-    };
-        let gist = await(
-          await centra("https://api.github.com/gists", "POST")
-          .header("User-Agent", "Malil")
-          .header("Authorization", `token ${process.env.gist}`)
-          .body(body, "json")
-          .send()
-        ).json();
-        const out = `https://gist.github.com/${gist.id}`
-          return out
-        }
+export const gist = async function(name: any, content: any) {
+	let files: { [key: string]: { content: string } } = {};
+	files[name] = {
+		content: content || "oops something went wrong :("
+	};
+	const body = {
+		description: `Malil`,
+		public: false,
+		files
+	};
+	let gist = await (await centra("https://api.github.com/gists", "POST")
+		.header("User-Agent", "Malil")
+		.header("Authorization", `token ${process.env.gist}`)
+		.body(body, "json")
+		.send()).json();
+	const out = `${gist.id}`;
+	return out;
+};
