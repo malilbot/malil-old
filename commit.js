4179c3df133c4fbca86a8abdb13cd96d6f1d7422
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-02-06 15:38:13 +0100

    better

--- commit.js
+++ commit.js
@@ -3,21 +3,25 @@ const {green, red, blue, yellow} = require("chalk")
 const {exec} = require("child_process");
 const {exit} = require('process');
 var rl = readline.createInterface({input: process.stdin, output: process.stdout});
-
+let out = ''
 rl.question(green("What is the commit message?:  "), function (answer) {
 
     exec(`git add --all`, async (error, stdout, stderr) => {
-        await console.log(await error || await stdout || await stderr)
+        out = error || stderr || stdout
     })
+    console.log(out)
     exec(`git commit -m "${answer}"`, async (error, stdout, stderr) => {
-        await console.log(await error || await stdout || await stderr)
+
+        out = error || stderr || stdout
+
     })
-    rl.question(green("DO you want to push?:  "), function (answer) {
+    rl.question(green(out + "\nDO you want to push?:  "), function (answer) {
         if (answer == 'yes' || answer == 'y') {
             exec(`git push`, async (error, stdout, stderr) => {
-                await console.log(await error || await stdout || await stderr)
+                out = error || stderr || stdout
+
             })
-            console.log(blue("Pushed the commit")) && exit(69)
+            console.log(blue(out + "\nPushed the commit")) && exit(69)
 
         } else if (answer == 'no' || answer == 'n') 
             return console.log(blue("Done")) && exit(69)
