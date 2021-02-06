52f199fb25a3b30fbeebfa97397b51b1ff1929ee
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-02-06 15:32:04 +0100

    added exit

--- commit.js
+++ commit.js
@@ -1,23 +1,34 @@
 var readline = require('readline');
 const {green, red, blue, yellow} = require("chalk")
-const { exec } = require("child_process");
-var rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+const {exec} = require("child_process");
+const {exit} = require('process');
+var rl = readline.createInterface({input: process.stdin, output: process.stdout});
 
-});
+rl.question(green("What is the commit message?:  "), function (answer) {
 
-rl.question(green("What is the commit message?:  "), function(answer) {
-
-            exec(`git add --all`, async (error, stdout, stderr) => {await console.log(error|| stdout|| stderr)})
-            exec(`git commit -m "${answer}"`, async (error, stdout, stderr) => {await console.log(error|| stdout|| stderr)})
-            rl.question(green("DO you want to push?:  "), function(answer) {
-              if(answer == 'yes' || answer == 'y'){
-                exec(`git push`, async (error, stdout, stderr) => {await console.log(error || stdout || stderr)})
-                console.log(blue("Done"))
-            } else if(answer == 'no' || answer == 'n') return console.log(blue("Done"))
-            else return console.log(yellow("I assume you dont want to push"))
+    exec(`git add --all`, async (error, stdout, stderr) => {
+        await console.log(await error || await stdout || await stderr)
+    })
+    exec(`git commit -m "${answer}"`, async (error, stdout, stderr) => {
+        await console.log(await error || await stdout || await stderr)
+    })
+    rl.question(green("DO you want to push?:  "), function (answer) {
+        if (answer == 'yes' || answer == 'y') {
+            exec(`git push`, async (error, stdout, stderr) => {
+                await console.log(await error || await stdout || await stderr)
             })
+            console.log(blue("Pushed the commit")) && exit(69)
+
+        } else if (answer == 'no' || answer == 'n') 
+            return console.log(blue("Done")) && exit(69)
+
+
+         else 
+            return console.log(yellow("I assume you dont want to push")) && exit(69)
+
+
+        
+
+
+    })
 })
-
-
