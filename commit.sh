23b0da48459bc23d05afc65172e0039534f8d411
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-02-06 11:03:26 +0100

    owo

--- commit.sh
+++ commit.sh
@@ -2,12 +2,13 @@ echo "Enter the commit message: "
 read a
 git add --all
 git commit -m $a
-echo "Want to push already? [yes/no]"
+echo "\e[0;31mWant to push already? [yes/no]"
 read e
 if [ "$e" = "yes" ] 
 then
 echo "Ok pushing"
 git push
+echo "Done!"
 else
 echo "Aight commited"
 fi
