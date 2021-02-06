09366476223acbcbac552934f86d88f05d861098
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-02-06 15:29:37 +0100

    format the code

--- commit.js
+++ commit.js
@@ -9,11 +9,11 @@ var rl = readline.createInterface({
 
 rl.question(green("What is the commit message?:  "), function(answer) {
 
-            exec(`git add --all`, async (error, stdout, stderr) => {console.log(error|| stdout|| stderr)})
-            exec(`git commit -m "${answer}"`, async (error, stdout, stderr) => {console.log(error|| stdout|| stderr)})
+            exec(`git add --all`, async (error, stdout, stderr) => {await console.log(error|| stdout|| stderr)})
+            exec(`git commit -m "${answer}"`, async (error, stdout, stderr) => {await console.log(error|| stdout|| stderr)})
             rl.question(green("DO you want to push?:  "), function(answer) {
               if(answer == 'yes' || answer == 'y'){
-                exec(`git push`, async (error, stdout, stderr) => {console.log(error || stdout || stderr)})
+                exec(`git push`, async (error, stdout, stderr) => {await console.log(error || stdout || stderr)})
                 console.log(blue("Done"))
             } else if(answer == 'no' || answer == 'n') return console.log(blue("Done"))
             else return console.log(yellow("I assume you dont want to push"))
