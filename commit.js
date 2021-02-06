8d53e5a2255e24a59df59d2f0a4486bd5928701e
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-02-06 15:42:17 +0100

    added a exit

--- commit.js
+++ commit.js
@@ -8,30 +8,26 @@ rl.question(green("What is the commit message?:  "), function (answer) {
 
     exec(`git add --all`, async (error, stdout, stderr) => {
         out = error || stderr || stdout
+        console.log(out)
     })
-    console.log(out)
-    eval(exec(`git commit -m "${answer}"`, async (error, stdout, stderr) => {
-
+    exec(`git commit -m "${answer}"`, async (error, stdout, stderr) => {
         out = error || stderr || stdout
-    }))
+        console.log(out)
+    })
     rl.question(green(out + "\nDO you want to push?:  "), function (answer) {
         if (answer == 'yes' || answer == 'y') {
             exec(`git push`, async (error, stdout, stderr) => {
                 out = error || stderr || stdout
-
             })
             console.log(blue(out + "\nPushed the commit")) && exit(69)
-
         } else if (answer == 'no' || answer == 'n') 
             return console.log(blue("Done")) && exit(69)
-
-
          else 
             return console.log(yellow("I assume you dont want to push")) && exit(69)
 
-
         
 
-
     })
+    process.exit(69)
+
 })
