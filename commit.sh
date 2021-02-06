53d8763063072bc3a0fb4ae0738f6ead0457d0ed
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-02-06 11:05:15 +0100

    added colors

--- commit.sh
+++ commit.sh
@@ -1,14 +1,14 @@
-echo "Enter the commit message: "
+echo "\e[4;32mEnter the commit message:\e[0m "
 read a
 git add --all
 git commit -m $a
-echo "\e[0;31mWant to push already? [yes/no]"
+echo "\e[0;31mWant to push already? [yes/no]\e[0m"
 read e
 if [ "$e" = "yes" ] 
 then
-echo "Ok pushing"
+echo "\e[0;32mOk pushing\e[0m"
 git push
-echo "Done!"
+echo "\e[0;35mDone!\e[0m"
 else
-echo "Aight commited"
+echo "\e[0;36mAight commited\e[0m"
 fi
