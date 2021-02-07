7192e53e7402b2850534a8fe23b339f21899e4b3
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-02-07 16:38:22 +0100

    owo

---
+++ file.js
@@ -0,0 +1,7 @@
+// AIzaSyDP9-qQ412wkigMkrqcIco6UpydCmxE6kY
+// key=yourAPIKey
+async function reee() {
+    const fetch = require("node-fetch")
+    console.log(await fetch('https://www.googleapis.com/customsearch/v1?key=AIzaSyDP9-qQ412wkigMkrqcIco6UpydCmxE6kYcx=017576662512468239146:omuauf_lfve&q=cars&callback=hndlr'))
+}
+reee()
---
+++ src/Listeners/Client/topgg.ts
@@ -0,0 +1,30 @@
+import { Listener } from "discord-akairo";
+import Client from "../../client/Client";
+import { MessageEmbed, TextChannel } from "discord.js";
+import fetch from "node-fetch";
+import * as Topgg from "@top-gg/sdk";
+
+export default class github extends Listener {
+	client: Client;
+	public constructor(client: Client) {
+		super("github", {
+			emitter: "client",
+			event: "ready",
+			category: "client"
+		});
+		this.client = client;
+	}
+
+	async exec() {
+		if (this.client.user.id == "800389986042118175") return;
+
+		const api = new Topgg.Api("Your top.gg token");
+
+		setInterval(() => {
+			api.postStats({
+				serverCount: this.client.guilds.cache.size,
+				shardCount: this.client.options.shardCount
+			});
+		}, 1800000);
+	}
+}
