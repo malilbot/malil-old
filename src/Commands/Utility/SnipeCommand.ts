fe08788d061f7e7dc4828772c4f117cb25c8748a
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-02-09 08:57:35 +0100

    added

--- src/Commands/Utility/SnipeCommand.ts
+++ src/Commands/Utility/SnipeCommand.ts
@@ -1,44 +1,46 @@
 import { Command } from "discord-akairo";
-import type { Message, GuildMember, ImageSize, AllowedImageFormat } from "discord.js";
 import { MessageEmbed } from "discord.js";
-import * as db from 'quick.db'
-import moment from 'moment';
+import * as db from "quick.db";
+import moment from "moment";
+import { TextChannel, Message } from "discord.js";
 
 export default class SnipeCommand extends Command {
-    public constructor() {
-        super("snipe", {
-            aliases: ["snipe"],
-            category: "Utility",
-            quoted: true,
-            args: [
-                {
-                    id: "args",
-                    type: "array",
-                    match: "rest",
-                }
-            ],
-            description: {
-                content: "",
-                usage: "snipe",
-                example: [
-                    "snipe"
-                ]
-            },
-            ratelimit: 3,
-            channel: "guild"
-        });
-    }
+	public constructor() {
+		super("snipe", {
+			aliases: [
+				"snipe"
+			],
+			category: "Utility",
+			quoted: true,
+			args: [
+				{
+					id: "args",
+					type: "array",
+					match: "rest"
+				}
+			],
+			description: {
+				content: "",
+				usage: "snipe",
+				example: [
+					"snipe"
+				]
+			},
+			ratelimit: 3,
+			channel: "guild"
+		});
+	}
 
-    public async exec(message: Message, { args }) {
-        let content = db.get(`snipe.${message.guild.id}.${message.channel.id}.content`)
-        if(!content) return message.reply("No Snipes found in this channel")
-        let author = db.get(`snipe.${message.guild.id}.${message.channel.id}.author`)
-        if(!author) return message.reply("No Snipes found in this channel")
-        const Embed = new MessageEmbed()
-        .setColor('#8E44AD')    
-        .setTitle(`Snipe: ${author}`)
-        .setDescription(`${content}`)
-        message.channel.send(Embed)
-
-    }
+	public async exec(message: Message, { args }) {
+		message.guild.channels.cache.forEach(async (element) => {
+			let ids = await (element as TextChannel).messages.fetch({ limit: 10 });
+			ids.forEach((element) => {});
+		});
+		let content = db.get(`snipe.${message.guild.id}.${message.channel.id}.content`);
+		if (!content) return message.reply("No Snipes found in this channel");
+		let author = db.get(`snipe.${message.guild.id}.${message.channel.id}.author`);
+		if (!author) return message.reply("No Snipes found in this channel");
+		const Embed = new MessageEmbed().setColor("#8E44AD").setTitle(`Snipe: ${author}`).setDescription(`${content}`);
+		message.channel.send(Embed);
+	}
 }
