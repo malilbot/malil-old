2402cbdf3e0883b9496f2d625bd4359eae44ef42
SkyBlockDev <itsdanielplay@outlook.com>
2021-02-14 11:09:32 +0100

    update added tasks

---
+++ src/Listeners/Client/taskHandler.ts
@@ -0,0 +1,37 @@
+import { Listener } from "discord-akairo";
+import { Collection } from "discord.js";
+import Client from "../../client/Client";
+import { join } from 'path';
+import ms from "ms"
+// import { server } from '../../Utils/Utils'
+
+
+export default class request extends Listener {
+    client: Client;
+    public constructor(client: Client) {
+        super("request", {
+            emitter: "client",
+            event: "ready",
+            category: "client"
+        });
+        this.client = client;
+    }
+
+    async exec() {
+        const fs = require('fs');
+        const tasks = new Collection()
+
+        const taskfiles = fs.readdirSync(join(__dirname, "..", "..", "tasks")).filter(file => file.endsWith('.js'));
+        for (const file of taskfiles) {
+            const task = require(join(__dirname, "..", "..", "tasks/" + file));
+            setInterval(() => {
+                task.execute();
+            }, (ms(task.delay)));
+
+
+        }
+
+
+
+    }
+}
