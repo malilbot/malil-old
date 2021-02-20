b0a216257e782041a7e61309a30b48701ec8a818
SkyBlockDev <itsdanielplay@outlook.com>
2021-02-20 09:22:49 +0100

    added guild blacklist cause a certain guild

---
+++ src/Inhibitors/GuildBlacklist.ts
@@ -0,0 +1,15 @@
+import { Inhibitor } from 'discord-akairo';
+import { Message } from 'discord.js';
+export default class extends Inhibitor {
+    constructor() {
+        super('GuildBlacklist', {
+            reason: 'blacklist',
+            type: 'all',
+        });
+    }
+
+    exec(message: Message) {
+        this.client.blacklist.ensure('blacklisted', [], 'guilds')
+        return (this.client.blacklist.get('blacklisted', 'guilds').includes(message.guild.id))
+    }
+}
