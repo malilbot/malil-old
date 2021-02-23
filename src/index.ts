a82c4ff72b8167df8b3947fe17c586985b2c9707
SkyBlockDev <itsdanielplay@outlook.com>
2021-02-23 20:44:53 +0100

    stuff

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import { token } from './config'
 import { ShardingManager } from 'discord.js';
 export const manager = new ShardingManager('./finish/bot.js', { token: token });
-manager.on('shardCreate', shard => console.log(`Launched shard ${shard.id}`));
+import { logger } from "./lib/Utils"
+manager.on('shardCreate', shard => logger.info(`[ SHARD STARTED ] ${shard.id}`));
 manager.spawn();
