09197997fafa3071aaefff8973f9c959a17b6461
SkyBlockDev <itsdanielplay@outlook.com>
2021-02-25 14:04:19 +0100

    remove code

--- src/Listeners/commandhandler/CommandStarted.ts
+++ src/Listeners/commandhandler/CommandStarted.ts
@@ -17,8 +17,5 @@ export default class CommandStarted extends Listener {
     exec(message, command) {
         this.client.gp.math("commands", "+", 1);
         this.client.logger.info(a1("[ COMMAND RAN ] ") + main(fixspace(command, 7)) + a1(" [ USER ] ") + sec(message.author.tag) + " " + sec(message.author.id) + a1(" [ GUILD ] ") + third(message.guild.name) + " " + third(message.guild.id))
-        // this.client.logger.info(`\x1b[32mCommand Used \x1b[33m- \x1b[32m${message.author.tag} (${message.author.id}) \x1b[33m- \x1b[32m${message.guild} (${message.guild.id}) \x1b[33m- \x1b[32m${command} \x1b[33m- \x1b[32m${message.id}`)
-        // this.client.logger.info(`\x1b[33m Command Used -\x1b[0m\x1b[32m: ${message.author.tag} \x1b[33m[GUILD]\x1b[0m\x1b[32m: ${message.guild.name} \x1b[33m[COMMAND]\x1b[0m\x1b[32m: ${command}`)
-        // this.client.logger.info(`\x1b[33m Command Used -\x1b[0m\x1b[32m: author id = '${message.author.id}' msg id =  '${message.id}' channel id = '${message.channel.id} guild id = '${message.guild.id}'`)
     }
 }
