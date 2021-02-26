9b3c60c80dca63874a56c0fcc4c7f3c55872226c
SkyBlockDev <itsdanielplay@outlook.com>
2021-02-26 21:59:28 +0100

    add topgg

--- src/Tasks/randomlists.ts
+++ src/Tasks/randomlists.ts
@@ -5,6 +5,16 @@ module.exports = {
     async execute(client) {
         if (client.user.id !== "749020331187896410") return;
 
+        const topgg = {
+            "server_count": client.guilds.cache.size,
+            "shard_count": client.shard.ids.length,
+            "shard_id": client.shard.ids[0]
+        }
+        await (await centra("https://top.gg/api//bots/749020331187896410/stats", "post")
+            .header("Authorization", client.setting.bottokens.topgg)
+            .body(topgg)
+            .send()).json();
+
         const discordbotlist = {
             "guilds": client.guilds.cache.size,
             "users": client.guilds.cache.reduce((a, g) => a + g.memberCount, 0),
@@ -18,7 +28,7 @@ module.exports = {
 
         const bladebotlist = {
             "server_count": client.guilds.cache.size,
-            "shardcount": 1
+            "shardcount": client.shard.ids.length
         }
         await (await centra("https://bladebotlist.xyz/api/bots/749020331187896410/stats", "post")
             .header("Authorization", client.setting.bottokens.Bladebnots)
@@ -28,7 +38,7 @@ module.exports = {
 
         const discordextremelist = {
             "guildCount": client.guilds.cache.size,
-            "shardcount": 1
+            "shardcount": client.shard.ids.length
         }
         await (await centra("https://api.discordextremelist.xyz/v2/bot/749020331187896410/stats", "post")
             .header("Authorization", client.setting.bottokens.discordextreme)
