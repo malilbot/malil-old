72fd0d1750c2569abf5d6980931c3920dbabe9b8
SkyBlockDev <itsdanielplay@outlook.com>
2021-02-27 20:14:30 +0100

    fancy stats

--- src/Tasks/stats.ts
+++ src/Tasks/stats.ts
@@ -11,20 +11,20 @@ module.exports = {
         const clientChannels = client.guilds.cache.reduce((a, b) => a + b.channels.cache.size, 0)
         const clientMembers = client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)
         if (client.shard.ids.length == 1) {
-            log(main(`Channels ${sec(clientChannels)}`))
-            log(main(`Members ${sec(clientMembers)}`))
-            log(main(`Guilds ${sec(clientGuilds)}`))
+            log(main(`[ CHANNELS ] ${sec(clientChannels)}`))
+            log(main(`[ MEMBERS  ] ${sec(clientMembers)}`))
+            log(main(`[ GUILDS   ] ${sec(clientGuilds)}`))
 
         } else {
             const totalGuilds = await client.shard.fetchClientValues('guilds.cache.size').then(serv => serv.reduce((acc, guildCount) => acc + guildCount, 0))
             const totalMembers = await client.shard.broadcastEval('this.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)').then(member => member.reduce((acc, memberCount) => acc + memberCount, 0))
             const totalChannels = await client.shard.broadcastEval('this.guilds.cache.reduce((acc, guild) => acc + guild.channels.cache.size, 0)').then(channel => channel.reduce((acc, channelCount) => acc + channelCount, 0))
-            log(main(`Channels       ${sec(clientChannels)}`))
-            log(main(`Members        ${sec(clientMembers)}`))
-            log(main(`Guilds         ${sec(clientGuilds)}`))
-            log(main(`Total Channels ${sec(totalChannels)}`))
-            log(main(`Total Members  ${sec(totalMembers)}`))
-            log(main(`Total Guilds   ${sec(totalGuilds)}`))
+            log(main(`[ CHANNELS ] ${sec(clientChannels)}`))
+            log(main(`[ MEMBERS  ] ${sec(clientMembers)}`))
+            log(main(`[ GUILDS   ] ${sec(clientGuilds)}`))
+            log(main(`[ TOTAL CHANNELS ] ${sec(totalChannels)}`))
+            log(main(`[ TOTAL MEMBERS  ] ${sec(totalMembers)}`))
+            log(main(`[ TOTAL GUILS    ] ${sec(totalGuilds)}`))
         }
 
     },
