b14e01f58663a835656a2e032c60149f3b943ffa
SkyBlockDev <itsdanielplay@outlook.com>
2021-03-06 18:13:58 +0100

    👑

--- settings.example.js
+++ settings.example.js
@@ -1,59 +1,19 @@
-// eslint-disable-next-line no-undef
-module.exports =
-{
-    "devtoken": "",
-
-    "token": "",
-
-    "prefix": "*",
-
-    "dev": true,
-
-    "rpc": {
-        "on": true,
-        "activity": {
-            "details": "Add malil bot or bad",
-            "assets": {
-                "large_image": "malil-pfp",
-                "large_text": "Coding bot"
-            },
-            "buttons": [{ label: "top.gg", url: "https://top.gg/bot/749020331187896410" }, { label: "invite", url: "https://discord.com/oauth2/authorize?client_id=749020331187896410&scope=bot&permissions=117824" }]
-        },
-        "clientId": ""
+export const credentials = {
+    "stuff goes here"
+};
+export const consts = {
+    colors: {
+        default: '#6df902',
+        red: '#ff0000',
+        green: '#00ff27',
+        purple: '#f000ff',
+        orange: '#ff7c00',
+        blue: '#0f00ff',
     },
-
-    "Monkey-speak": "I dont need a src folder",
-
-    "genius": "",
-
-    "owners": "336465356304678913",
-
-    "TestServer": "755166643927122091",
-
-    "topgg": "",
-
-    "gist": "gist authorization token",
-
-    "mongoPath": "",
-    "presence": {
-        "status": "online",
-        "games": [
-            {
-                "type": "PLAYING",
-                "name": "Prefix * or @ me",
-                "url": ""
-            }
-        ],
-        "interval": 6000
-    },
-    "language": "English",
-    "colors": {
-        "default": "#6df902",
-        "red": "#ff0000",
-        "green": "#00ff27",
-        "purple": "#f000ff",
-        "orange": "#ff7c00",
-        "blue": "#0f00ff"
-    }
-}
-
+};
+export const setting = {
+    owners: ['336465356304678913'],
+    dev: true,
+    rpc: true,
+    prefix: 'm-',
+};
