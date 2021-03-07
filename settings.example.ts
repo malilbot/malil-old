66fbbb11f5b1d02f91ff6c152d6e8b78e43cb278
SkyBlockDev <itsdanielplay@outlook.com>
2021-03-07 09:47:52 +0100

    updated settings example

---
+++ settings.example.ts
@@ -0,0 +1,37 @@
+export const credentials = {
+	/** I would suggest you to split long tokens with + to make them format better */
+	devtoken: '',
+
+	token: '',
+
+	genius: '',
+	dagpi: '',
+
+	bottokens: {
+		discordbotlist: '',
+		Bladebnots: '',
+		topgg: '',
+		discordextreme: '',
+		botsgg: '',
+	},
+	TestServer: '',
+
+	gist: '',
+	mongoPath: '',
+};
+export const consts = {
+	colors: {
+		default: '#6df902',
+		red: '#ff0000',
+		green: '#00ff27',
+		purple: '#f000ff',
+		orange: '#ff7c00',
+		blue: '#0f00ff',
+	},
+};
+export const setting = {
+	owners: ['336465356304678913'],
+	dev: true,
+	rpc: true,
+	prefix: '-',
+};
