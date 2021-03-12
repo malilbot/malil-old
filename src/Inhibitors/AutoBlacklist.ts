46ee545c791d3970b8270609fad338d7c1a649fa
SkyBlockDev <itsdanielplay@outlook.com>
2021-03-12 23:38:31 +0100

    blacklist logging

--- src/Inhibitors/AutoBlacklist.ts
+++ src/Inhibitors/AutoBlacklist.ts
@@ -1,5 +1,6 @@
 import { Inhibitor, Command } from 'discord-akairo';
 import { Message } from 'discord.js';
+import { main, sec, third, fourth, split } from '../lib/Utils';
 import { superUsers } from '../lib/config';
 const timeoutObject = {};
 const blacklist = {};
@@ -15,7 +16,6 @@ export default class extends Inhibitor {
 	public async exec(message: Message): Promise<boolean> {
 		if (superUsers.includes(message.author.id)) return false;
 		if (blacklist[message.author.id] == true) return true;
-		console.log(timeoutObject);
 		if (!timeoutObject[message.author.id]) {
 			timeoutObject[message.author.id] = 0;
 		}
@@ -26,16 +26,27 @@ export default class extends Inhibitor {
 			timeoutObject[message.author.id] = num - 1;
 		}, 600000);
 		if (num == 60) {
+			this.client.logger.info(
+				third(
+					'[ BLACKLISTED USER ] ' +
+						sec(`[ ${message.author.tag} ] [ ${message.author.id}]`)
+				)
+			);
 			timeoutObject[message.author.id] = 0;
 			blacklist[message.author.id] = true;
 			message.reply(
 				'You have been blacklisted for 24 hours undereview for abusing the bot, To apeal your ban do fire new in the support server https://discord.gg/mY8zTARu4g'
 			);
 			setTimeout(function () {
+				this.client.logger.info(
+					third(
+						'[ UN-BLACKLISTED USER ] ' +
+							sec(`[ ${message.author.tag} ] [ ${message.author.id}]`)
+					)
+				);
 				blacklist[message.author.id] = false;
 			}, 86400000);
 		}
-		console.log(timeoutObject);
 		return false;
 	}
 }
