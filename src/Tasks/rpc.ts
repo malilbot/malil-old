add0b90dc71c13361f4e8a08d9277dc7509343f7
SkyBlockDev <itsdanielplay@outlook.com>
2021-03-15 17:16:15 +0100

    stuff

--- src/Tasks/rpc.ts
+++ src/Tasks/rpc.ts
@@ -1,39 +1,39 @@
-import * as DiscordRPC from "discord-rpc"
-import { main, sec, third, fourth, a1, split } from '../lib/Utils';
+import * as DiscordRPC from "discord-rpc";
+import { main, sec, third, fourth, a1, split } from "../lib/Utils";
 module.exports = {
-	name: 'rpc',
-	delay: '30m',
+	name: "rpc",
+	delay: "30m",
 	runOnStart: true,
 	awaitReady: true,
 	execute(client) {
 		if (client.settings.rpc == true) {
 			try {
-				console.log(fourth("[ CONNECTING TO RPC ]"))
-				const clientId = '795717859170844673';
+				console.log(fourth("[ CONNECTING TO RPC ]"));
+				const clientId = "795717859170844673";
 				DiscordRPC.register(clientId);
 
-				const rpc = new DiscordRPC.Client({ transport: 'ipc' });
+				const rpc = new DiscordRPC.Client({ transport: "ipc" });
 				rpc.login({ clientId });
-				rpc.on('ready', () => {
+				rpc.on("ready", () => {
 					rpc.setActivity({
 						buttons: [
 							//prettier-ignore
-							//{ label: 'Support', url: 'https://discord.gg/mY8zTARu4g' },
+							{ label: 'Support', url: 'https://discord.gg/mY8zTARu4g' },
 							//prettier-ignore
-							{ label: 'google', url: 'https://google.com' },
-							//{ label: 'invite', url:'https://discord.com/oauth2/authorize?client_id=749020331187896410&scope=bot&permissions=117824', },
+							//{ label: 'google', url: 'https://google.com' },
+							{ label: 'invite', url:'https://discord.com/oauth2/authorize?client_id=749020331187896410&scope=bot&permissions=117824', },
 						],
-						largeImageKey: 'pfp2',
-						largeImageText: 'CODING BOT',
-						smallImageKey: 'betapfp2',
-						smallImageText: 'why are you looking here invite it already',
-						state: 'Invite MALIL for cookies',
+						largeImageKey: "pfp2",
+						largeImageText: "CODING BOT",
+						smallImageKey: "betapfp2",
+						smallImageText: "why are you looking here invite it already",
+						state: "Invite MALIL for cookies",
 						instance: false,
-					})
-					console.log(fourth("[ CONNECTED TO RPC ]"))
+					});
+					console.log(fourth("[ CONNECTED TO RPC ]"));
 				});
 			} catch (e) {
-				console.log(fourth('[ COULD NOT CONNECT TO RPC ]'));
+				console.log(fourth("[ COULD NOT CONNECT TO RPC ]"));
 			}
 		}
 	},
