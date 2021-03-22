aa536b376dc43a2f76e1ca280562b97f2ff256d4
SkyBlockDev <itsdanielplay@outlook.com>
2021-03-22 14:24:05 +0100

    you now get blacklisted 24 times faster

--- src/Inhibitors/AutoBlacklist.ts
+++ src/Inhibitors/AutoBlacklist.ts
@@ -25,8 +25,8 @@ export default class extends Inhibitor {
 		setTimeout(function () {
 			const num = timeoutObject[message.author.id];
 			timeoutObject[message.author.id] = num - 1;
-		}, 600000);
-		if (num == 60) {
+		}, 1200000);
+		if (num == 30) {
 			const { GStr, UStr } = Format(message, null, null, null);
 			this.client.logger.info(a1(`[ USER ] ${UStr} [ GUILD ] ${GStr} [ BLACKLISTED ]`));
 			timeoutObject[message.author.id] = 0;
@@ -34,10 +34,7 @@ export default class extends Inhibitor {
 			message.reply(
 				new MessageEmbed()
 					.setImage("https://http.cat/423")
-					.addField(
-						"You have been blacklisted from using this bot for 24 hours",
-						"If you believe this is a mistake visit the support [server](https://discord.gg/mY8zTARu4g)"
-					)
+					.addField("You have been blacklisted from using this bot for 24 hours", "If you believe this is a mistake visit the support [server](https://discord.gg/mY8zTARu4g)")
 					.setColor(this.client.consts.colors.purple)
 			);
 			setTimeout(function () {
