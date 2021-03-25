9fb3b1fa49534af5cf2f40ae6045ff61ec5b7b12
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-03-25 17:02:25 +0100

    final finishes on reload command

--- src/Commands/Developer/ReloadCommand.ts
+++ src/Commands/Developer/ReloadCommand.ts
@@ -32,18 +32,29 @@ export default class ReloadCommand extends Command {
 			} else {
 				_stdout = "```" + stdout + "```\n";
 			}
-			embed.addField("\u200B", "Running git pull <:github:824673035499733022>\n" + _stdout);
+			let ghMSG = "Running git pull <:github:824673035499733022>\n";
+			if (stdout.includes("Already up to date.")) {
+				_stdout = "";
+				ghMSG = "Git pulled no new commits <:github:824673035499733022>\n";
+			}
+			embed.addField("\u200B", ghMSG + _stdout);
 			msg.edit({ embed, allowedMentions: { repliedUser: false } });
-
-			exec("yarn rm", () => {
-				embed.addField("\u200B", "Deleting the Dist folder <:join_arrow:824668785163894845>");
-				msg.edit({ embed, allowedMentions: { repliedUser: false } });
-
+			if (_stdout == "") {
 				exec("npx tsc", async () => {
 					embed.addField("\u200B", "Reloading <a:updating:824662408239906897>");
 					await msg.edit({ embed, allowedMentions: { repliedUser: false } }).then(() => this.client.shard.respawnAll());
 				});
-			});
+			} else {
+				exec("yarn rm", () => {
+					embed.addField("\u200B", "Deleting the Dist folder <:join_arrow:824668785163894845>");
+					msg.edit({ embed, allowedMentions: { repliedUser: false } });
+
+					exec("npx tsc", async () => {
+						embed.addField("\u200B", "Reloading <a:updating:824662408239906897>");
+						await msg.edit({ embed, allowedMentions: { repliedUser: false } }).then(() => this.client.shard.respawnAll());
+					});
+				});
+			}
 		});
 	}
 }
