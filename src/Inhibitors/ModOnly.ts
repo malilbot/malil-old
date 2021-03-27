38708b36546ffe7df2e11da551fc821787ebd21e
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-03-27 20:28:33 +0100

    fix

--- src/Inhibitors/ModOnly.ts
+++ src/Inhibitors/ModOnly.ts
@@ -13,7 +13,7 @@ export default class extends Inhibitor {
 	public async exec(message: Message): Promise<boolean> {
 		if (superUsers.includes(message.author.id)) return false;
 		if (message.member.permissions.has("MANAGE_MESSAGES")) return false;
-
+		this.client.guilddata.ensure(message.guild.id, [], "modonly");
 		const channels = this.client.guilddata.get(message.guild.id, "modonly");
 		if (!channels) return false;
 
