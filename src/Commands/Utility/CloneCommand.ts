655d7541f3489fe3046329e32f118b23fecb5311
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-03-27 08:52:59 +0100

    added clone emoji command

--- src/Commands/Utility/CloneCommand.ts
+++ src/Commands/Utility/CloneCommand.ts
@@ -1,7 +1,6 @@
 import { Command } from "discord-akairo";
-import type { Message, GuildMember, ImageSize, AllowedImageFormat } from "discord.js";
-import { MessageEmbed } from "discord.js";
-import { fixword } from "../../lib/Utils";
+import type { Message } from "discord.js";
+import { Util } from "discord.js";
 export default class CloneCommand extends Command {
 	public constructor() {
 		super("clone", {
@@ -22,10 +21,33 @@ export default class CloneCommand extends Command {
 			},
 			ratelimit: 3,
 			channel: "guild",
-			userPermissions: ["MANAGE_MESSAGES"],
-			clientPermissions: ["MANAGE_MESSAGES", "SEND_MESSAGES"],
+			userPermissions: ["MANAGE_EMOJIS"],
+			clientPermissions: ["MANAGE_EMOJIS", "SEND_MESSAGES"],
 		});
 	}
 
-	public async exec(message: Message, { args }): Promise<void> {}
+	public async exec(message: Message, { args }: { args: string }): Promise<void> {
+		if (!args.startsWith("<:") || !args.endsWith(">")) {
+			message.reply("Thats not a emoji");
+			return;
+		}
+		const customEmo = Util.parseEmoji(args);
+		if (!customEmo) {
+			message.reply("Atleast give a valid emoji");
+			return;
+		}
+		const link = `https://cdn.discordapp.com/emojis/${customEmo.id}.${customEmo.animated ? "gif" : "png"}`;
+
+		const emoname = customEmo.name;
+		if (!customEmo) {
+			message.reply("Atleast give a valid emoji");
+			return;
+		}
+		if (message.guild.emojis.cache.find((emoji) => emoji.id === customEmo.id)) {
+			message.channel.send("You cant clone a emoji you already have that would be weird");
+			return;
+		}
+		const emoji = message.guild.emojis.create(`${link}`, `${emoname}`);
+		message.channel.send(`Cloned ${await emoji} enjoy!`);
+	}
 }
