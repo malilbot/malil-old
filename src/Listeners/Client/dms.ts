9ba9d79660795d4126e8f1ffd1b1d4fd3b2207a8
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-03-27 14:57:19 +0100

    special code

--- src/Listeners/Client/dms.ts
+++ src/Listeners/Client/dms.ts
@@ -19,6 +19,15 @@ export default class message extends Listener {
 	}
 
 	async exec(message: Message): Promise<void | Logger> {
+		let spokenusers = [];
+
+		if (message.channel.id == "807302538558308355") {
+			if (spokenusers.includes(message.author.id)) return;
+			spokenusers.push(message.author.id);
+			const reg = /^<@!?304054669372817419> happy birthday <:clownmonke:807704911973187585>$/i;
+			if (message.content.search(reg) == -1) message.delete();
+		}
+
 		if (message.channel.id == "823935750168117312") {
 			if (message.webhookID) {
 				try {
