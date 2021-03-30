bd85d0f86bac58b3b26b80cac84b0e405bc51fcb
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-03-30 18:30:38 +0200

    fixed load

--- src/Commands/Developer/LoadCommand.ts
+++ src/Commands/Developer/LoadCommand.ts
@@ -1,7 +1,7 @@
 import { Command } from "discord-akairo";
 import { MessageEmbed, Message } from "discord.js";
 import { exec } from "child_process";
-import { cpuUsage, fixspace, ms, Util } from "../../lib/Utils";
+import { ms, Util } from "../../lib/Utils";
 import os from "os";
 export default class LoadCommand extends Command {
 	public constructor() {
@@ -30,44 +30,37 @@ export default class LoadCommand extends Command {
 	// eslint-disable-next-line @typescript-eslint/explicit-module-boundary-types
 	public async exec(message: Message): Promise<void> {
 		const ut = new Util();
-		//prettier-ignore
-		const totalGuilds = await this.client.shard
-			.fetchClientValues("guilds.cache.size")
-			.then((serv) => serv.reduce((acc, guildCount) => acc + guildCount, 0));
+
+		const totalGuilds = await this.client.shard.fetchClientValues("guilds.cache.size").then((serv) => serv.reduce((acc, guildCount) => acc + guildCount, 0));
 		const totalMembers = await this.client.shard
 			.broadcastEval("this.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)")
 			.then((member) => member.reduce((acc, memberCount) => acc + memberCount, 0));
-		cpuUsage((v: number) => {
-			exec("ps -e | wc -l", async (error, stdout) => {
-				const processes = stdout;
-				exec("free -m | grep Mem", async (error, stdout) => {
-					const memory = stdout.trim().split(" ")[20];
-					exec("cat /sys/class/thermal/thermal_zone0/temp", async (error, stdout) => {
-						const temps = Number(stdout);
-						const sysStats =
-							"```\n" +
-							`Uptime ${ms(os.uptime() * 1000, { long: true })}, processes ${processes}` +
-							`CPU Used: ${(v * 100).toFixed(1)}%, Heat ${temps / 1000} C\n` +
-							`Ram used: ${memory} MB\n` +
-							"```\n";
-						const botStats =
-							"```\n" +
-							`Bot Uptime: ${ms(process.uptime() * 1000, { long: true })}` +
-							`Memory Used: ${ut.formatBytes(process.memoryUsage().heapUsed)}\n` +
-							`Guilds: ${totalGuilds}, Members: ${totalMembers}\n` +
-							`Commands Used: ${this.client.gp.get("commands")}\n` +
-							"```\n";
-						const embed = new MessageEmbed()
-							.setFooter(message.author.tag)
-							.setThumbnail(this.client.user.avatarURL())
-							.setColor(this.client.consts.colors.default)
-							.addField(`☆ Bot Stats ${this.client.user.username}`, botStats)
-							.addField(`☆ System stats`, sysStats);
-						message.util.send(embed);
-					});
+		//cpuUsage((v: number) => {
+		exec("ps -e | wc -l", async (error, stdout) => {
+			const processes = stdout;
+			exec("free -m | grep Mem", async (error, stdout) => {
+				const memory = stdout.trim().split(" ")[20];
+				exec("cat /sys/class/thermal/thermal_zone0/temp", async (error, stdout) => {
+					const temps = Number(stdout);
+					const sysStats = "```\n" + `Uptime ${ms(os.uptime() * 1000, { long: true })}, processes ${processes}` + `CPU Heat ${temps / 1000} C\n` + `Ram used: ${memory} MB\n` + "```\n";
+					const botStats =
+						"```\n" +
+						`Bot Uptime: ${ms(process.uptime() * 1000, { long: true })}` +
+						`Memory Used: ${ut.formatBytes(process.memoryUsage().heapUsed)}\n` +
+						`Guilds: ${totalGuilds}, Members: ${totalMembers}\n` +
+						`Commands Used: ${this.client.gp.get("commands")}\n` +
+						"```\n";
+					const embed = new MessageEmbed()
+						.setFooter(message.author.tag)
+						.setThumbnail(this.client.user.avatarURL())
+						.setColor(this.client.consts.colors.default)
+						.addField(`☆ Bot Stats ${this.client.user.username}`, botStats)
+						.addField(`☆ System stats`, sysStats);
+					message.util.send(embed);
 				});
 			});
 		});
+		//});
 	}
 }
 /*
