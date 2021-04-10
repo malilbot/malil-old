085c796752408058b4335439949f24ed21d07e26
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-04-10 19:00:56 +0200

    xml > html

--- src/Lib/parseMd.ts
+++ src/Lib/parseMd.ts
@@ -4,7 +4,7 @@ import marked from "marked";
 for (const file of readdirSync(join(__dirname, "..", "..", "public", "md")).filter((file) => file.endsWith(".md"))) {
 	writeFileSync(
 		join(__dirname, "..", "..", "public", "html", file.replace(".md", ".html")),
-		readFileSync(join(__dirname, "..", "..", "public", "md", file.replace(".md", ".htm")), "utf-8").replace(
+		readFileSync(join(__dirname, "..", "..", "public", "md", file.replace(".md", ".xml")), "utf-8").replace(
 			"{{input}}",
 			marked(readFileSync(join(__dirname, "..", "..", "public", "md", file), "utf-8"))
 		)
