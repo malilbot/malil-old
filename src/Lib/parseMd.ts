8eda0b29f4e13f7cb8a766f83d1b41a0ac1ee816
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-04-10 19:34:48 +0200

    Add prettier to html and markdown files

--- src/Lib/parseMd.ts
+++ src/Lib/parseMd.ts
@@ -1,13 +1,15 @@
 import { readFileSync, writeFileSync, readdirSync } from "fs";
 import { join } from "path";
 import marked from "marked";
+import { exec } from "child_process";
 for (const file of readdirSync(join(__dirname, "..", "..", "public", "md")).filter((file) => file.endsWith(".md"))) {
 	writeFileSync(
 		join(__dirname, "..", "..", "public", "html", file.replace(".md", ".html")),
-		readFileSync(join(__dirname, "..", "..", "public", "md", file.replace(".md", ".xml")), "utf-8").replace(
+		readFileSync(join(__dirname, "..", "..", "public", "md", file.replace(".md", ".html")), "utf-8").replace(
 			"{{input}}",
 			marked(readFileSync(join(__dirname, "..", "..", "public", "md", file), "utf-8"))
 		)
 	);
 	console.log(file);
 }
+exec("npx prettier-eslint --write ./public/**/**");
