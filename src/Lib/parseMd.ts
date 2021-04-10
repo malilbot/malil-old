2628a60ab5903ebb5ae61ce59e488d5b2300529f
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-04-10 20:47:26 +0200

    quotes

--- src/Lib/parseMd.ts
+++ src/Lib/parseMd.ts
@@ -2,12 +2,15 @@ import { readFileSync, writeFileSync, readdirSync } from "fs";
 import { join } from "path";
 import marked from "marked";
 import { exec } from "child_process";
+const showdown = require("showdown"),
+	converter = new showdown.Converter();
+converter.setFlavor("github");
 for (const file of readdirSync(join(__dirname, "..", "..", "public", "md")).filter((file) => file.endsWith(".md"))) {
 	writeFileSync(
 		join(__dirname, "..", "..", "public", "html", file.replace(".md", ".html")),
 		readFileSync(join(__dirname, "..", "..", "public", "md", file.replace(".md", ".html")), "utf-8").replace(
 			"{{input}}",
-			marked(readFileSync(join(__dirname, "..", "..", "public", "md", file), "utf-8"))
+			converter.makeHtml(readFileSync(join(__dirname, "..", "..", "public", "md", file), "utf-8"))
 		)
 	);
 	console.log(file);
