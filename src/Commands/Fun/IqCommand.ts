a13dff5ae06747430128ea3ac9e0aac5d3420ed2
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-04-13 17:40:25 +0200

    Add a easteregg to iq command

--- src/Commands/Fun/IqCommand.ts
+++ src/Commands/Fun/IqCommand.ts
@@ -27,17 +27,11 @@ export default class IqCommand extends Command {
 
 	public async exec(message: Message): Promise<void> {
 		const member: GuildMember = (await GetMember(message)) || message.member;
-		let iq: number;
-		this.client.UserData.ensure(member.id, {
-			iq: 0,
-		});
-		if (this.client.UserData.get(member.id, "iq")) {
-			iq = this.client.UserData.get(member.id, "iq");
-		} else {
-			iq = Math.floor(Math.random() * 150) + 1;
-			this.client.UserData.set(member.id, iq, "iq");
-		}
+		const iq: number = this.client.UserData.ensure(member.id, Math.floor(Math.random() * 150) + 1, "iq");
+
 		const iEmbed = new MessageEmbed().setColor(this.client.consts.colors.default).setTitle("IQ Test").setDescription(`${member}'s IQ is: \`${iq}\`!`);
+
+		if (Math.floor(Math.random() * 40 + 1) == 5) iEmbed.setImage("https://i.imgur.com/skuWtMT.png");
 		if (Math.floor(Math.random() * 10 + 1) == 5)
 			iEmbed.setFooter(`You can vote to get increased iq ${(await this.client.prefixes.get(message.guild.id, "prefix")) || this.client.settings.prefix}vote`);
 		message.util.send(iEmbed);
