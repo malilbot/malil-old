95429168fe90b17227668ab4293d6e1e87df0f71
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-04-16 09:56:51 +0200

    Site improvements

--- src/Lib/parseMd.ts
+++ src/Lib/parseMd.ts
@@ -8,35 +8,60 @@ showdown.setFlavor("github");
 const topnav = readFileSync(join(__dirname, "..", "..", "public", "src", "global", "topnav.as"), "utf-8");
 const head = readFileSync(join(__dirname, "..", "..", "public", "src", "global", "head.as"), "utf-8");
 const breef = readFileSync(join(__dirname, "..", "..", "wiki", "Breef.md"), "utf-8");
-for (const file of readdirSync(join(__dirname, "..", "..", "public", "src", "scss")).filter((file) => file.endsWith(".scss"))) {
-	console.time(cyan(file));
-	const scss = join(__dirname, "..", "..", "public", "src", "scss", file);
-	const css = join(__dirname, "..", "..", "public", "css", file.replace(".scss", ".css"));
-	exec(`yarn sass ${scss} ${css}`);
-	console.timeEnd(cyan(file));
-}
 
-for (const file of readdirSync(join(__dirname, "..", "..", "public", "src", "md")).filter((file) => file.endsWith(".tf"))) {
-	console.time(blue(file));
-	const html = readFileSync(join(__dirname, "..", "..", "public", "src", "md", file), "utf-8").replace("{{breef}}", breef).split("{!")[0];
-	const md = readFileSync(join(__dirname, "..", "..", "public", "src", "md", file), "utf-8").replace("{{breef}}", breef).split("{!")[1];
-	const converted = showdown.makeHtml(md);
-	//todo time
-	const dir = join(__dirname, "..", "..", "public", "html", file.replace(".tf", ".html"));
+const compiler = new (class Compiler {
+	scssdir: string;
+	tfdir: string;
+	cssdir: string;
+	htmldir: string;
+	constructor({ scss, tf, css, html }: { scss: string; tf: string; css: string; html: string }) {
+		this.scssdir = scss;
+		this.tfdir = tf;
+		this.cssdir = css;
+		this.htmldir = html;
+	}
+	async compileScss() {
+		for (const file of readdirSync(this.scssdir).filter((file) => file.endsWith(".scss"))) {
+			console.time(cyan(file));
+			const scss = join(this.scssdir, file);
+			const css = join(this.cssdir, file.replace(".scss", ".css"));
+			exec(`yarn sass ${scss} ${css}`);
+			console.timeEnd(cyan(file));
+		}
+	}
+	async compileTf() {
+		for (const file of readdirSync(join(this.tfdir)).filter((file) => file.endsWith(".tf"))) {
+			console.time(blue(file));
+			const html = readFileSync(join(this.tfdir, file), "utf-8").split("{!")[0];
+			const md = readFileSync(join(this.tfdir, file), "utf-8").split("{!")[1];
+			const converted = showdown.makeHtml(md);
+			const dir = join(this.htmldir, file.replace(".tf", ".html"));
 
-	const out = html
-		.replace("{{input}}", converted)
+			const out = html
+				.replace("{{input}}", converted)
 
-		.replace("{{topnav}}", topnav)
+				.replace("{{topnav}}", topnav)
 
-		.replace("{{head}}", head)
+				.replace("{{head}}", head)
 
-		.replace("{{breef}}", breef)
+				.replace("{{breef}}", breef)
 
-		.replace("{{title}}", file.replace(".tf", ""));
+				.replace("{{title}}", file.replace(".tf", ""));
 
-	writeFileSync(dir, out);
-	console.timeEnd(blue(file));
-}
-console.time(green("Formatted"));
-//exec("npx prettier-eslint --write ./public/**/**", () => console.timeEnd(green("Formatted")));
+			writeFileSync(dir, out);
+			console.timeEnd(blue(file));
+		}
+	}
+	async compile() {
+		this.compileScss();
+		this.compileTf();
+		console.time(green("Formatted"));
+		//exec("npx prettier-eslint --write ./public/**/**", () => console.timeEnd(green("Formatted")));
+	}
+})({
+	scss: join(__dirname, "..", "..", "public", "src", "scss"),
+	tf: join(__dirname, "..", "..", "public", "src", "md"),
+	css: join(__dirname, "..", "..", "public", "css"),
+	html: join(__dirname, "..", "..", "public", "html"),
+});
+compiler.compile();
