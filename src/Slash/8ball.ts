1919a19a79a9d5af4c89a60b04e97f45a1e29119
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-04-16 17:39:46 +0200

    Add more langs to 8ball

--- src/Slash/8ball.ts
+++ src/Slash/8ball.ts
@@ -18,56 +18,9 @@ export default class ballCommand extends Command {
 	}
 
 	async exec(message: CommandInteraction) {
-		const replies = [
-			"yes.",
-			"no.",
-			"maybe.",
-			"yes an' definitely.",
-			"it be certain.",
-			"as I spy it, aye.",
-			"very doubtful.",
-			"eh I will say aye to that there.",
-			"no!",
-			"yes - definitely.",
-			"most likely.",
-			"never.",
-			"better not tell ye now",
-			"my sources say no",
-			"nope.",
-			"scientifically aye.",
-			"yes.",
-			"no.",
-			"maybe.",
-			"yes and definitewy.",
-			"it is cewtain.",
-			"as i see it, yes.",
-			"vewy douwbtfuww.",
-			"eh i wiww say yes to that.",
-			"no!",
-			"yes - definitewy.",
-			"most wikewy.",
-			"nevew.",
-			"bettew not teww u now",
-			"my souwwces say no",
-			"nope.",
-			"scientificawwy yes.",
-			"Yes.",
-			"No.",
-			"Maybe.",
-			"Yes and definitely.",
-			"It is certain.",
-			"As I see it, yes.",
-			"Very doubtful.",
-			"Eh I will say yes to that.",
-			"NO!",
-			"Yes - definitely.",
-			"Most likely.",
-			"Never.",
-			"Better not tell you now",
-			"My sources say no",
-			"Nope.",
-			"Scientifically yes.",
-		];
+		//prettier-ignore
+		const replies =
+			"yes.|no.|maybe.|yes an' definitely.|it be certain.|as I spy it, aye.|very doubtful.|eh I will say aye to that there.|no!|yes - definitely.|most likely.|never.|better not tell ye now|my sources say no|nope.|scientifically aye.|yes.|no.|maybe.|yes and definitewy.|it is cewtain.|as i see it, yes.|vewy douwbtfuww.|eh i wiww say yes to that.|no!|yes - definitewy.|most wikewy.|nevew.|bettew not teww u now|my souwwces say no|nope.|scientificawwy yes.|YEZ.|NO.|MAYBE.|YEZ AN DEFINITELY.|IT CERTAIN.|AS I C IT, YEZ.|VRY DOUBTFUL.|EH IM GONNA SAY YEZ 2 DAT.|NO!|YEZ - DEFINITELY.|MOST LIKELY.|NEVR.|BETTR NOT TELL U NAO|MAH SOURCEZ SAY NO|NOPE.|SCIENTIFICALLY YEZ.|Yes.|No.|Maybe.|Yes and definitely.|It is certain.|As I see it, yes.|Very doubtful.|Eh I will say yes to that.|NO!|Yes - definitely.|Most likely.|Never.|Better not tell you now|My sources say no|Nope.|Scientifically yes.".split("|")
 
 		const result = Math.floor(Math.random() * replies.length);
 
