3391ea7a29e90dbfa1ee07f50ce883585e9c7312
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-04-17 10:22:07 +0200

    Pat command

---
+++ src/Commands/Fun/PatCommand.ts
@@ -0,0 +1,51 @@
+import { Command } from "discord-akairo";
+import { MessageEmbed, Message, GuildMember } from "discord.js";
+import { fixword, GetMember } from "../../Lib/Utils";
+import c from "centra";
+import petPetGif from "pet-pet-gif";
+export default class PatCommand extends Command {
+	public constructor() {
+		super("pat", {
+			aliases: ["pat"],
+			category: "Fun",
+			quoted: true,
+			args: [
+				{
+					id: "member",
+					type: async (message, content) => {
+						console.log(content);
+						let member = await GetMember(message, content);
+						if (member) return member;
+						else return content;
+					},
+					match: "content",
+				},
+			],
+			description: {
+				content: "You can pat a minecraft head or a discord user with this command",
+				usage: "pat",
+				example: ["pat"],
+			},
+			clientPermissions: ["SEND_MESSAGES"],
+			ratelimit: 3,
+			channel: "guild",
+		});
+	}
+
+	public async exec(message: Message, { member }: { member: string | GuildMember }): Promise<Message> {
+		let image: string;
+		console.log(member);
+		if (typeof member == "string") {
+			const res = await (await c("https://api.mojang.com/users/profiles/minecraft/" + member, "GET").send()).json();
+			if (res !== null) {
+				image = `https://crafatar.com/renders/head/${res.id}`;
+			} else {
+				return message.reply("User not found");
+			}
+		} else {
+			image = (member as GuildMember).user.displayAvatarURL({ dynamic: false, format: "png" });
+		}
+
+		return message.reply({ content: "patting", files: [{ attachment: await petPetGif(image), name: `patted.gif` }] });
+	}
+}
