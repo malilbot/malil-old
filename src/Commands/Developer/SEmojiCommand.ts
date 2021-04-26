c16ea4804191302d0181b73ea2186a63ce801267
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-04-26 17:08:44 +0200

    semoji

---
+++ src/Commands/Developer/SEmojiCommand.ts
@@ -0,0 +1,36 @@
+import { Command } from "discord-akairo";
+
+import { Message } from "discord.js";
+import { fixword } from "../../Lib/Utils";
+export default class semojiCommand extends Command {
+	public constructor() {
+		super("semoji", {
+			aliases: ["semoji"],
+			category: "Developer",
+			description: {
+				content: "force the bot to semoji stuff",
+				usage: "semoji",
+				example: ["semoji pog"],
+			},
+			ratelimit: 3,
+			args: [
+				{
+					id: "emoji",
+					type: "string",
+					match: "rest",
+				},
+			],
+			superUserOnly: true,
+			channel: "guild",
+		});
+	}
+
+	public async exec(message: Message, { emoji }: { emoji: string }): Promise<void> {
+		try {
+			message.delete().catch(() => {});
+		} catch (e) {}
+		const str = `${this.client.emojis.cache.find((e) => e.name.includes(emoji))}`;
+		if (str == "undefined") return;
+		message.channel.send(str);
+	}
+}
