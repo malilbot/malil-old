6fa7a7700df8920ccd49eb2b8ad10e4eeee5964d
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-04-28 17:38:03 +0200

    made quote command nicer

--- src/Commands/Utility/QuoteCommand.ts
+++ src/Commands/Utility/QuoteCommand.ts
@@ -32,6 +32,7 @@ export default class QuoteCommand extends Command {
 	}
 
 	public async exec(message: Message, { args, force }: { args: string; force: boolean }): Promise<Message> {
+		//todo fancy quotes
 		if (!args) return message.util.send("please add a message link");
 		if (!message.content.includes("/")) return message.util.send("Please add a message link");
 
@@ -40,23 +41,23 @@ export default class QuoteCommand extends Command {
 		if (!split[5] || !split[6]) {
 			return message.util.send("message not found");
 		}
-		let chan;
+		let fetched: TextChannel;
 		let msg: Message;
 
 		try {
-			chan = await this.client.channels.fetch(split[5]);
-			msg = await (chan as TextChannel).messages.fetch(split[6]);
+			fetched = (await this.client.channels.fetch(split[5])) as TextChannel;
+			msg = await fetched.messages.fetch(split[6]);
 		} catch (error) {
 			return message.util.send("message not found");
 		}
 
-		let url = "";
+		let url = [];
 		if (msg.attachments) {
 			msg.attachments.forEach((attachment) => {
-				url = attachment.url;
+				url.push(attachment.url);
 			});
 		}
-		if ((chan as TextChannel).nsfw == true) {
+		if (fetched.nsfw == true) {
 			if (!force) {
 				return message.util.send("nsfw");
 			} else if (!this.client.gp.get("superUsers").includes(message.author.id) && !this.client.settings.owners.includes(message.author.id)) {
@@ -64,12 +65,15 @@ export default class QuoteCommand extends Command {
 			}
 		}
 
-		let attachment: unknown;
-		if (url) attachment = await new MessageAttachment(url);
+		let attachment: MessageAttachment;
+		if (url[0]) attachment = await new MessageAttachment(url[0]);
 		if (!message.member.guild.me.permissions.has(["MANAGE_WEBHOOKS"])) {
-			return message.util.send(
-				new MessageEmbed().setImage(url).setAuthor(msg.author.tag, msg.author.avatarURL()).setDescription(msg.content).setFooter("didnt have WebhookPermissions so send a embed instead")
-			);
+			const guild = this.client.guilds.cache.get("836982936100274216");
+			let emoji = guild.emojis.cache.find((emoji) => emoji.name == msg.author.username);
+			if (!emoji) emoji = await guild.emojis.create(msg.author.avatarURL(), msg.author.username /** preventing api errors */);
+			if (!msg.content && msg.embeds) return message.channel.send(msg.embeds) && message.channel.send("Command triggered by " + message.author.tag);
+			if (url) return message.channel.send({ content: `${emoji}${msg.author.tag} once said\n> ${msg.content}`, files: url });
+			else return message.channel.send({ content: `${emoji}${msg.author.tag} once said\n> ${msg.content}` });
 		}
 		const name = msg.webhookID ? msg.author.username : msg.author.tag;
 		const webhook = await (message.channel as TextChannel).createWebhook(name).then((webhook) =>
