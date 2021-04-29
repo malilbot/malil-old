5f3dd97b038b210a29bf7f79579ddf730c2ea90a
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-04-29 12:27:21 +0200

    Dungeon rooms

--- src/Lib/dungeonroom.ts
+++ src/Lib/dungeonroom.ts
@@ -2,37 +2,57 @@ import type { Message } from "discord.js";
 const talkedRecently = new Set();
 export = async (message: Message) => {
 	if (!talkedRecently.has(message.author.id)) {
-		const check = (i: string) =>
-			message.content
-				.toLowerCase()
-				.replace(/[^a-z0-9]+|\s+/gim, "")
-				.includes(i);
+		if (message.channel.id == "807702096064937990") return;
 
-		let smh: Message;
-		if (check("bannable") && check("this")) {
-			talkedRecently.add(message.author.id);
-			smh = await message.reply("The mod is not bannable and doesnt trigger watchdog.");
-		} else if (check("remove") || check("close") || check("rid") || check("next") || check("change")) {
-			if (check("pic") || check("image") || check("secret")) {
-				talkedRecently.add(message.author.id);
-				smh = await message.reply(
-					"To remove the SBP secret images, you have to press a hotkey (which is configurable in the Minecraft controls menu). Default keys are O to open images, B for previous image, N for next image, and M to clear/remove images from the screen."
-				);
+		const check = (i: string | string[]) => {
+			const ls = (m: string) =>
+				message.content
+					.toLowerCase()
+					.replace(/[^a-z0-9]+|\s+/gim, "")
+					.includes(m);
+			if (typeof i == "string") {
+				return ls(i);
+			} else if (typeof i == "object") {
+				for (const str of i) {
+					if (ls(str)) {
+						return true;
+					} else {
+						continue;
+					}
+				}
 			}
+		};
+		let smh: Message;
+		if (check("bann")) {
+			talkedRecently.add(message.author.id);
+			smh = await message.reply({
+				files: ["http://pays.host/uploads/add4657d-af3a-4f66-a67f-605109f80024/bzxrcnWt.png"],
+				content: "The mod is not bannable and doesnt trigger watchdog.",
+			});
+		} else if (check(["next", "change", "move", "close", "rid"]) && check(["image", "pic"])) {
+			talkedRecently.add(message.author.id);
+			smh = await message.reply({
+				content:
+					"To remove the SBP secret images, you have to press a hotkey (which is configurable in the Minecraft controls menu). Default keys are O to open images, B for previous image, N for next image, and M to clear/remove images from the screen.",
+			});
+		} else if (check(["how", "get", "where"]) && check(["sbp", "skyblockplus"])) {
+			talkedRecently.add(message.author.id);
+			smh = await message.reply({
+				content:
+					"https://discord.gg/2UjaFqfPwJ\n\n" +
+					"**Make sure you download both normal SBP and the Dungeon Secrets Mod**\n" +
+					"Dungeon Secrets Mod is NOT this mod, this mod is Dungeon Rooms Mod.\n" +
+					"Again, in addition to the Dungeon Rooms Mod you downloaded from this server, you need to download **2 mods** from the Skyblock Personalized Discord server",
+			});
 		} else return;
 
-		if (smh) {
-			const filter = (reaction) => {
-				return reaction.emoji.name === "❌";
-			};
-			await smh.react("❌");
-			smh.awaitReactions(filter, { max: 1, time: 60000 /** 60 seconds */ }).then((collected) => {
-				if (!collected.first()) return;
-				if (collected.first()) {
-					smh.delete();
-				}
-			});
-		}
+		await smh.react("❌");
+		smh.awaitReactions((reaction) => reaction.emoji.name === "❌", { max: 1, time: 60000 /** 60 seconds */ }).then((collected) => {
+			if (!collected.first()) return;
+			if (collected.first()) {
+				smh.delete();
+			}
+		});
 
 		setTimeout(() => {
 			talkedRecently.delete(message.author.id);
