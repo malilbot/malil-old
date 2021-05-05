908562c0cfe0af875dc724f4a5f704016b941f9b
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-05-05 15:44:59 +0200

    fixed a typo on yt

--- src/Commands/Fun/YoutubeCommand.ts
+++ src/Commands/Fun/YoutubeCommand.ts
@@ -33,7 +33,7 @@ export default class YoutubeCommand extends Command {
 	}
 	async execSlash(message: CommandInteraction) {
 		const channel = message.options[0]?.channel;
-		if (channel.type !== "voice") return message.reply("This command can only be used on a textchannel");
+		if (channel.type !== "voice") return message.reply("This command can only be used on a VoiceChannel");
 		if (!channel.guild.me.permissions.has("CREATE_INSTANT_INVITE")) return message.reply("I need the **create invite** permission for this command to work");
 		//@ts-expect-error
 		this.client.api //@ts-expect-error
