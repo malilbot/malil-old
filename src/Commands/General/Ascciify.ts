fff2c2792eed588061b0a065b21606160bdd3448
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-05-13 12:21:13 +0200

    switch ascciify to api

--- src/Commands/General/Ascciify.ts
+++ src/Commands/General/Ascciify.ts
@@ -1,19 +1,12 @@
 import Command from "../../Classes/malilCommand";
 import { GuildMember, Message, MessageAttachment } from "discord.js";
-import asciify from "asciify-image";
 import { hst, GetMember } from "../../Lib/Utils";
+import c from "centra";
 export default class AsciifyCommand extends Command {
 	public constructor() {
 		super("asciify", {
 			aliases: ["asciify", "ascii", "assci", "asccii", "asci", "aci"],
 			args: [
-				{
-					id: "big",
-					type: "boolean",
-					flag: "--big",
-					match: "flag",
-				},
-
 				{
 					id: "member",
 					type: async (message, content) => {
@@ -35,43 +28,23 @@ export default class AsciifyCommand extends Command {
 		});
 	}
 
-	public async exec(message: Message, { big, member }: { big: boolean; member: GuildMember }): Promise<Message> {
-		const options = {
-			fit: "box",
-			width: 64,
-			height: 64,
-			color: false,
-		};
-		const bigoptions = {
-			fit: "box",
-			width: 128,
-			height: 128,
-			color: false,
-		};
+	public async exec(message: Message, { member }: { member: GuildMember }): Promise<Message> {
 		let url: string | MessageAttachment;
-		let text = true;
 
 		message.attachments.forEach((attachment) => {
 			url = attachment;
 		});
 		if (!url) {
-			text = false;
 			url = member.user.displayAvatarURL({
 				size: 512,
 				format: "png",
-				dynamic: true,
+				dynamic: false,
 			});
 		}
 		if (!url) return message.util.send("please add a image attachment");
-		const option = big ? bigoptions : options;
 
-		asciify(url, option, async function (err: Error, asciified: string) {
-			if (err) return message.util.send("Unsupported file type");
-			let sentence: string;
-			if (text == true) sentence = "Success! " + (await hst(asciified)) + ".txt";
-			else if (text == false) sentence = "Success! " + (await hst(asciified)) + ".txt \nProtip: You can add a image and it will asiifyy that";
-			else sentence = "Idk what happend but no image arrived";
-			message.util.send(sentence, { allowedMentions: { repliedUser: false } });
-		});
+		const gif = await (await c("https://pet.skyblockdev.repl.co/api/asciify/", "GET").query("url", url).send()).json();
+		const link = await hst(gif.out);
+		message.util.send("Success " + link, { allowedMentions: { repliedUser: false } });
 	}
 }
