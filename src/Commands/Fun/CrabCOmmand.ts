c28c32171a42b2018764bd7470197d294bc3a6a4
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-05-27 15:51:52 +0200

    crab command

---
+++ src/Commands/Fun/CrabCOmmand.ts
@@ -0,0 +1,29 @@
+import Command from "../../Classes/malilCommand";
+import { fixword } from "../../Lib/Utils";
+import { Message } from "discord.js";
+export default class CrabCommand extends Command {
+	constructor() {
+		super("crab", {
+			aliases: ["clap"],
+			category: "Fun",
+			quoted: true,
+			args: [
+				{
+					id: "args",
+					type: "array",
+					match: "rest",
+					default: "Me When No Arguments",
+				},
+			],
+			description: "",
+			clientPermissions: ["SEND_MESSAGES"],
+			ratelimit: 3,
+			channel: "guild",
+		});
+	}
+
+	async exec(message: Message, { args }: { args: string }): Promise<void> {
+		//🦀
+		message.util.send("🦀" + (await fixword(args.replace("/s+/g", "🦀").split(" ").join("🦀"))) + "🦀");
+	}
+}
