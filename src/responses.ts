58f7989d6fa9394f7c5a6401a8852fc718065111
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-05-28 16:38:16 +0200

    send crash

--- src/responses.ts
+++ src/responses.ts
@@ -52,6 +52,17 @@ export default [
 	{
 		guilds: [`${skytils}`],
 		message: {
+			content:
+				`Win key + R and then type %appdata%\\.minecraft\\crash-reports\n` +
+				`mac: ~/Library/Application Support/minecraft\n` +
+				`Linux: You should know this if your using linux\n` +
+				`https://cdn.discordapp.com/attachments/807302782432051230/845361400288772126/2021-04-29T13-29-47.mp4`,
+		},
+		triggers: [[`send`], ["crash"]],
+	},
+	{
+		guilds: [`${skytils}`],
+		message: {
 			content: `https://streamable.com/1rauw6`,
 		},
 		triggers: [
