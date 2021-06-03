84bc9b9721b2c9be9c25cfa6747408e15632ac3d
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-06-03 14:20:11 +0200

    smh discordjs errors

--- src/Commands/Information/StatusCommand.ts
+++ src/Commands/Information/StatusCommand.ts
@@ -52,33 +52,39 @@ export default class StatsCommand extends Command {
 					.setThumbnail(this.client.user.displayAvatarURL())
 					.setTimestamp()
 					.setColor(message.guild.me.displayHexColor || "RED")
-					.addField("General", [
-						`**● Name:** ${this.client.user.tag} (${this.client.user.id})`,
-						`**● Servers:** ${this.client.guilds.cache.size}`,
-						`**● Users:** ${this.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)}`,
-						`**● Channels:** ${this.client.guilds.cache.reduce((a, b) => a + b.channels.cache.size, 0)}`,
-						`**● Creation Date:** ${utc(this.client.user.createdTimestamp).format("Do MMMM YYYY HH:mm:ss")}`,
-						`**● Bot Uptime:** ${ms(process.uptime() * 1000, { long: true })}`,
-						`**● Owner:** <@!336465356304678913>`,
-						`**● Support:** [Support server](https://discord.gg/mY8zTARu4g)`,
-						`**● Commands ran:** ${this.client.gp.get("commands")}`,
-						`**● Node.js:** ${process.version}`,
-						`**● Akairo:** ${akairov}`,
-						`**● Discord.js:** ${djsversion}`,
-						"\u200b",
-					])
-					.addField("System", [
-						`**● Platform** Ubuntu Server (pi)`,
-						`**● Uptime:** ${ms(os.uptime() * 1000, { long: true })}`,
-						`**● CPU:**`,
-						`\u3000 Cores: ${os.cpus().length}`,
-						`\u3000 Model: ${core.model}`,
-						`\u3000 Speed: ${core.speed}MHz`,
-						`**● Memory:**`,
-						`\u3000 Total: ${ut.formatBytes(process.memoryUsage().heapTotal)}`,
-						`\u3000 Used: ${ut.formatBytes(process.memoryUsage().heapUsed)}`,
-						"\u200b",
-					])
+					.addField(
+						"General",
+						[
+							`**● Name:** ${this.client.user.tag} (${this.client.user.id})`,
+							`**● Servers:** ${this.client.guilds.cache.size}`,
+							`**● Users:** ${this.client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)}`,
+							`**● Channels:** ${this.client.guilds.cache.reduce((a, b) => a + b.channels.cache.size, 0)}`,
+							`**● Creation Date:** ${utc(this.client.user.createdTimestamp).format("Do MMMM YYYY HH:mm:ss")}`,
+							`**● Bot Uptime:** ${ms(process.uptime() * 1000, { long: true })}`,
+							`**● Owner:** <@!336465356304678913>`,
+							`**● Support:** [Support server](https://discord.gg/mY8zTARu4g)`,
+							`**● Commands ran:** ${this.client.gp.get("commands")}`,
+							`**● Node.js:** ${process.version}`,
+							`**● Akairo:** ${akairov}`,
+							`**● Discord.js:** ${djsversion}`,
+							"\u200b",
+						].join("\n")
+					)
+					.addField(
+						"System",
+						[
+							`**● Platform** Ubuntu Server (pi)`,
+							`**● Uptime:** ${ms(os.uptime() * 1000, { long: true })}`,
+							`**● CPU:**`,
+							`\u3000 Cores: ${os.cpus().length}`,
+							`\u3000 Model: ${core.model}`,
+							`\u3000 Speed: ${core.speed}MHz`,
+							`**● Memory:**`,
+							`\u3000 Total: ${ut.formatBytes(process.memoryUsage().heapTotal)}`,
+							`\u3000 Used: ${ut.formatBytes(process.memoryUsage().heapUsed)}`,
+							"\u200b",
+						].join("\n")
+					)
 			);
 		}
 	}
