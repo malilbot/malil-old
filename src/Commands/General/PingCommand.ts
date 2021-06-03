b324d238629366f00d590189a5b0aef2318de6fc
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-06-03 14:26:20 +0200

    remvoe logging from ping

--- src/Commands/General/PingCommand.ts
+++ src/Commands/General/PingCommand.ts
@@ -18,16 +18,14 @@ export default class PingCommand extends Command {
 		const afterQueryTime = Date.now();
 
 		message.reply("pinging").then((m) => {
-			console.log(message);
-			console.log(message.createdTimestamp);
-			console.log(m.createdTimestamp);
 			const embed = this.client.util
 				.embed()
 				.setColor("GREEN")
 				.addField("Discord latency", `${m.createdTimestamp - message.createdTimestamp || "UNLIMITED POWER!"} ms`)
 				.addField("Api latency", `${Math.round(this.client.ws.ping) || "UNLIMITED POWER!"} ms`)
 				.addField("Database latency", `${afterQueryTime - beforeQueryTime || "UNLIMITED POWER!"} ms`);
-			m.edit({ embed: embed, content: "" });
+
+			m.edit({ embed: embed, content: "pong" });
 		});
 	}
 }
