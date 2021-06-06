5ede1fafb1ab03003a15ab3ff059693b94bb4ed8
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-06-06 13:07:18 +0200

    Now logging slash errors

--- src/Listeners/slash/slashError.ts
+++ src/Listeners/slash/slashError.ts
@@ -1,7 +1,7 @@
-import { Interaction } from "discord.js";
-import { Listener } from "discord-akairo";
+import { Listener, AkairoMessage } from "discord-akairo";
 import Command from "../../Classes/malilCommand";
 
+import { hst } from "../../Lib/Utils";
 export default class CommandErrorListener extends Listener {
 	constructor() {
 		super("slashError", {
@@ -11,8 +11,27 @@ export default class CommandErrorListener extends Listener {
 		});
 	}
 
-	exec(error: Error): void {
-		this.client.logger.fatal(Interaction);
-		this.client.logger.fatal(error);
+	async exec(error: Error, message: AkairoMessage, command: Command): Promise<void> {
+		const errorEmbed = this.client.util
+			.embed()
+
+			.setDescription(
+				`**User:** ${message.author} (${message.author.tag})\n` +
+					`**Command:** ${command}\n` +
+					`**Channel:** ${message.channel} (${message.channel.id})\n` +
+					`**Message:** [link](${message.url})\n`
+			)
+			.addField("Error", `${await hst(error.stack)}`)
+			.setTimestamp();
+		const errrorr = this.client.util
+			.embed()
+			.setColor("RED")
+			.addField("ERROR", "```js\n" + `${error.stack.slice(0, 1000)}` + "```");
+		this.client.webhook.send({ embeds: [errorEmbed, errrorr] });
+		this.client.logger.error("──────────────────────────────────────────────────────────────────────");
+		this.client.logger.command(message, command, "Error");
+		this.client.logger.error(`[ CONTENT ] ${message.content}`);
+		this.client.logger.fatal(error.stack);
+		this.client.logger.error("──────────────────────────────────────────────────────────────────────");
 	}
 }
