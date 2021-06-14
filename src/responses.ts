c0558c9068d25d9465fb79f4399a5f387038a50d
SkyBlockDev <72335827+SkyBlockDev@users.noreply.github.com>
2021-06-14 06:47:27 +0200

    Update responses.ts

--- src/responses.ts
+++ src/responses.ts
@@ -22,11 +22,6 @@ export default [
 		triggers: [["aliases"]],
 	},
 	{
-		guilds: [skytils],
-		delete: true,
-		triggers: [["sel"], ["offers", "ah", "auc"]],
-	},
-	{
 		guilds: [`${dg}`],
 		message: {
 			content: `This mod, like all others is use at your own risk. Even though it is *use at your own risk*, there have no reports of users being banned from the mod, and it should follow all of the current hypixel mod rules.`,
